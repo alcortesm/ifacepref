cf65eda9e4363a0f669f408d5d99e9f5a9e075fe
Alberto Cortés <alcortes@it.uc3m.es>
2009-04-10 16:04:45 +0200

    add proper handling of static major

--- src/ifacepref.c
+++ src/ifacepref.c
@@ -7,32 +7,45 @@ MODULE_VERSION("0.2");
 
 const char * const NAME="ifacepref";
 const unsigned int DEV_COUNT = 1;
-dev_t major = 0;
+const int MAJOR = 0; /* 0 means dynamic allocation */
+const int MINOR = 0;
+dev_t dev;
 
 static int
 ifacepref_init(void)
 {
     int error;
+    int major;
     
-    printk(KERN_ALERT "ifacepref init\n");
+    printk(KERN_ALERT "IFACEPREF ifacepref_init() entering\n");
 
-    /* major number allocation (dynamic or static) */
+    /* major number allocation */
+    major = MAJOR;
     if (!major)
-        error = alloc_chrdev_region(&major, 0, DEV_COUNT, NAME); /* first minor is 0 */    
-    else
-        error = register_chrdev_region(major, DEV_COUNT, NAME); 
+        error = alloc_chrdev_region(&dev, MINOR, DEV_COUNT, NAME);
+    else {
+        dev = MKDEV(major, MINOR);
+        error = register_chrdev_region(dev, DEV_COUNT, NAME);
+    }
     if (error)
         return -1;
+    printk(KERN_ALERT "IFACEPREF ifacepref_init() registered at major=%d and minor=%d\n",
+            MAJOR(dev), MINOR(dev));
 
+    printk(KERN_ALERT "IFACEPREF ifacepref_init() leaving\n");
     return 0;
 }
 
 static void ifacepref_exit(void)
 {
-    printk(KERN_ALERT "ifacepref exit\n");
+    printk(KERN_ALERT "IFACEPREF ifacepref_exit() entering\n");
 
     /* free allocated device numbers */
-    unregister_chrdev_region(major, DEV_COUNT); 
+    unregister_chrdev_region(dev, DEV_COUNT); 
+    printk(KERN_ALERT "IFACEPREF ifacepref_exit() unregister major=%d and minor=%d\n",
+            MAJOR(dev), MINOR(dev));
+
+    printk(KERN_ALERT "IFACEPREF ifacepref_exit() leaving\n");
 }
 
 module_init(ifacepref_init);
