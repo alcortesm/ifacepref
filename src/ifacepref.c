e315dc3cca9afdb926b8c1f07d93b5631970a35a
Alberto Cortés <alcortes@it.uc3m.es>
2009-04-16 18:13:01 +0200

    Add PDEBUG macro
    The PDEBUG macro can be used instead of printk( KERN_DEBUG and
    can be easily disabled by a Makefile variable.

--- src/ifacepref.c
+++ src/ifacepref.c
@@ -7,6 +7,21 @@
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.4");
 
+/* define PDEBUG & PDEBUGG */
+#undef PDEBUG
+#ifdef IFACEPREF_DEBUG
+#  ifdef __KERNEL__
+#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "ifacepref: " fmt, ## args)
+#  else
+#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
+#  endif
+#else
+#  define PDEBUG(fmt, args...) /* not debugging */
+#endif
+
+#undef PDEBUGG
+#define PDEBUGG(fmt, args...) /* placeholder */
+
 const char * const NAME="ifacepref";
 const unsigned int DEV_COUNT = 1;
 const unsigned int MAJOR = 0; /* 0 means dynamic allocation */
@@ -39,9 +54,9 @@ ifacepref_init(void)
     unsigned int major;
     int i;
     
-    printk(KERN_ALERT "IFACEPREF ifacepref_init() entering\n");
+    PDEBUG("ifacepref_init() entering\n");
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_open() initializing ifacepref to empty string\n");
+    PDEBUG("ifacepref_init() initializing ifacepref to empty string\n");
     for (i=0; i<IFNAMSIZ; i++)
         buffer[i] = '\0';
     content_end =  buffer;
@@ -56,7 +71,7 @@ ifacepref_init(void)
     }
     if (err)
         return -1;
-    printk(KERN_ALERT "IFACEPREF ifacepref_init() registered at major=%u, minor=%u, count=%u\n",
+    PDEBUG("ifacepref_init() registered at major=%u, minor=%u, count=%u\n",
             MAJOR(devnum), MINOR(devnum), DEV_COUNT);
 
     /* char device registration */
@@ -65,53 +80,53 @@ ifacepref_init(void)
     cdevp->owner = THIS_MODULE;
     err = cdev_add(cdevp, devnum, DEV_COUNT);
     if (err) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_init() err adding char device to major=%u, minor=%u, count=%u\n",
+        PDEBUG("ifacepref_init() err adding char device to major=%u, minor=%u, count=%u\n",
                 MAJOR(devnum), MINOR(devnum), DEV_COUNT);
         unregister_chrdev_region(devnum, DEV_COUNT);
-        printk(KERN_ALERT "IFACEPREF ifacepref_init() unregister major=%u, minor=%u, count=%u\n",
+        PDEBUG("ifacepref_init() unregister major=%u, minor=%u, count=%u\n",
                 MAJOR(devnum), MINOR(devnum), DEV_COUNT);
         return -1;
     }
-    printk(KERN_ALERT "IFACEPREF ifacepref_init() char device added to major=%u, minor=%u, count=%u\n",
+    PDEBUG("ifacepref_init() char device added to major=%u, minor=%u, count=%u\n",
             MAJOR(devnum), MINOR(devnum), DEV_COUNT);
     
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_init() leaving\n");
+    PDEBUG("ifacepref_init() leaving\n");
     return 0;
 }
 
 static void
 ifacepref_exit(void)
 {
-    printk(KERN_ALERT "IFACEPREF ifacepref_exit() entering\n");
+    PDEBUG("ifacepref_exit() entering\n");
 
     /* free allocated device numbers */
     unregister_chrdev_region(devnum, DEV_COUNT); 
-    printk(KERN_ALERT "IFACEPREF ifacepref_exit() unregister major=%u, minor=%u, count=%u\n",
+    PDEBUG("ifacepref_exit() unregister major=%u, minor=%u, count=%u\n",
             MAJOR(devnum), MINOR(devnum), DEV_COUNT);
 
     /* unregister char device */
-    printk(KERN_ALERT "IFACEPREF ifacepref_exit() unregistering char device major=%u, minor=%u, count=%u...",
+    PDEBUG("ifacepref_exit() unregistering char device major=%u, minor=%u, count=%u...",
             MAJOR(cdevp->dev), MINOR(cdevp->dev), cdevp->count);
     cdev_del(cdevp);
-    printk(KERN_ALERT "done\n");
+    PDEBUG("done\n");
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_exit() leaving\n");
+    PDEBUG("ifacepref_exit() leaving\n");
 }
 
 int
 ifacepref_open(struct inode *inodep, struct file * filp)
 {
-    printk(KERN_ALERT "IFACEPREF ifacepref_open() entering\n");
-    printk(KERN_ALERT "IFACEPREF ifacepref_open() leaving\n");
+    PDEBUG("ifacepref_open() entering\n");
+    PDEBUG("ifacepref_open() leaving\n");
     return 0;
 }
 
 int
 ifacepref_release(struct inode *inodep, struct file * filp)
 {
-    printk(KERN_ALERT "IFACEPREF ifacepref_release() entering\n");
-    printk(KERN_ALERT "IFACEPREF ifacepref_release() leaving\n");
+    PDEBUG("ifacepref_release() entering\n");
+    PDEBUG("ifacepref_release() leaving\n");
     return 0;
 }
 
@@ -123,12 +138,12 @@ ifacepref_read(struct file * filp, char __user *user_buff, size_t count, loff_t 
     size_t read_count;
     int pending;
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_read() entering count=%u, offset=%llu\n",
+    PDEBUG("ifacepref_read() entering count=%u, offset=%llu\n",
             count, *offp);
 
     /* input sanity check */
     if (*offp < 0) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_read() ERROR negative offset provided\n");
+        PDEBUG("ifacepref_read() ERROR negative offset provided\n");
         return -EINVAL;
     }
 
@@ -142,7 +157,7 @@ ifacepref_read(struct file * filp, char __user *user_buff, size_t count, loff_t 
 
     /* out of bound checks on read region */
     if (start > content_end) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_read() offset bigger than content size -> EOF; leaving with 0\n");
+        PDEBUG("ifacepref_read() offset bigger than content size -> EOF; leaving with 0\n");
         return 0;
     }
     if (end > content_end)
@@ -152,15 +167,15 @@ ifacepref_read(struct file * filp, char __user *user_buff, size_t count, loff_t 
 
     pending = copy_to_user(user_buff, (const void *)(start), read_count);
     if (pending) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_read() ERROR not valid user-space pointer\n");
+        PDEBUG("ifacepref_read() ERROR not valid user-space pointer\n");
         return -EFAULT;
     }
 
     *offp += read_count;
-    printk(KERN_ALERT "IFACEPREF ifacepref_read() %u bytes of data copied to user-space\n",
+    PDEBUG("ifacepref_read() %u bytes of data copied to user-space\n",
             read_count);
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_read() leaving with %u\n", read_count);
+    PDEBUG("ifacepref_read() leaving with %u\n", read_count);
     return read_count;
 }
 
@@ -172,12 +187,12 @@ ifacepref_write(struct file * filp, const char __user *user_buff, size_t count, 
     size_t write_count;
     int pending;
 
-    printk(KERN_ALERT "IFACEPREF ifacepref_write() entering count=%u, offset=%llu\n",
+    PDEBUG("ifacepref_write() entering count=%u, offset=%llu\n",
             count, *offp);
    
    /* input sanity checks */
    if (*offp != 0) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_write() ERROR requested offset is not 0\n");
+        PDEBUG("ifacepref_write() ERROR requested offset is not 0\n");
         return -EFBIG;
    }
 
@@ -191,7 +206,7 @@ ifacepref_write(struct file * filp, const char __user *user_buff, size_t count, 
 
    /* sanity checks on write region */
    if (end > buffer_end) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_write() ERROR requested offset is not 0\n");
+        PDEBUG("ifacepref_write() ERROR requested offset is not 0\n");
         return -ENOSPC;
    }
 
@@ -200,14 +215,14 @@ ifacepref_write(struct file * filp, const char __user *user_buff, size_t count, 
 
     pending = copy_from_user(start, user_buff, write_count);
     if (pending) {
-        printk(KERN_ALERT "IFACEPREF ifacepref_write() ERROR not valid user-space pointer\n");
+        PDEBUG("ifacepref_write() ERROR not valid user-space pointer\n");
         return -EFAULT;
     }
-    printk(KERN_ALERT "IFACEPREF ifacepref_write() %u bytes of data copied from user-space\n",
+    PDEBUG("ifacepref_write() %u bytes of data copied from user-space\n",
             write_count);
 
     *offp += write_count;
-    printk(KERN_ALERT "IFACEPREF ifacepref_write() leaving with %u\n",
+    PDEBUG("ifacepref_write() leaving with %u\n",
             write_count);
     return write_count;
 }
